924741b89276a7d07898522d9a969190124df676
Matheus Silva <matheusp.silva@outlook.com.br>
2022-12-27 22:28:48 -0300

    criacao do modulo de usuario com login e cadastro

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('livro/', include('livro.urls'))
+    path('livro/', include('livro.urls')),
+    path('auth/', include('usuarios.urls')),
+
 ]
--- livro/models.py
+++ livro/models.py
@@ -1,16 +1,19 @@
 from django.db import models
-
+from django.utils.timezone import now
 
 class Livros(models.Model):
     nome = models.CharField(max_length=100)
     autor = models.CharField(max_length=30)
     co_autor = models.CharField(max_length=30, blank=True, null=True)
-    data_cadastro = models.DateTimeField()
+    data_cadastro = models.DateTimeField(default=now())
     emprestado = models.BooleanField(default=False)
-    nome_emprestado = models.CharField(max_length=30, blank=True)
+    nome_emprestado = models.CharField(max_length=30, blank=True, null=True)
     data_emprestado = models.DateTimeField(blank=True, null=True)
     data_devolucao = models.DateTimeField(blank=True, null=True)
     tempo_duracao = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         verbose_name = 'Livro'
+
+    def __str__(self):
+        return self.nome
